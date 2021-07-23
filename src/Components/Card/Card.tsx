e02d53433c8f76de24d493f6ce71cc3b6cacba8c
Sebastian <tassone_af@yahoo.com>
2021-07-22 21:14:20 -0300

    Add: card component

---
+++ src/Components/Card/Card.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Card({ artist }: any): JSX.Element {
+
+  return (
+    <div className="card">
+      <img src="..." className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  )
+}
